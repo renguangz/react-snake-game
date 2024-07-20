d6c70536c0f8b3abff0a5d03aaf212ee82d3bc33
renguangz <rg870328@gmail.com>
2024-07-21 00:50:21 +0800

    feat: init

---
+++ config/config.ts
@@ -0,0 +1,3 @@
+export const config = {
+  api_domain: "https://hahow-recruit.herokuapp.com",
+};
---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import StyledComponentsRegistry from "@/utils/registry";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
+    </html>
+  );
+}
